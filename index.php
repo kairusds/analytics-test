470cc263dd2341477b0a7a77b5803776f41750e9
kairusds <harveyhansjavier@gmail.com>
2021-09-17 01:09:15 +0800

    Proceed

--- index.php
+++ index.php
@@ -1,62 +1,6 @@
 <?php
 
-$url = getenv("JAWSDB_URL");
-$dbparts = parse_url($url);
-$hostname = $dbparts["host"];
-$username = $dbparts["user"];
-$password = $dbparts["pass"];
-$database = ltrim($dbparts["path"], "/");
-
-$dbconn = mysqli_connect($hostname, $username, $password);
-
-if(!$dbconn){
-	die("Failed to connect to MySQL: " . mysqli_connect_error());
-}
-mysqli_select_db($dbconn, $database);
-
-function query($conn, $sql){
-	$res = null;
-	try{
-		$res = mysqli_query($conn, $sql);
-	}catch(Exception $e){
-		echo $e->getMessage();
-	}
-	return $res;
-}
-
-query($dbconn, "set names \"utf8\"");
-
-$create_query = <<<SQL
-CREATE TABLE IF NOT EXISTS `bbl_analytics` (
-  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `device_model` VARCHAR(50) NOT NULL,
-  `region` varchar(3) DEFAULT "cn",
-  `sdk_version` TINYINT(4) DEFAULT "1"
-) ENGINE="MyISAM" DEFAULT CHARSET="utf8";
-SQL;
-
-query($dbconn, $create_query);
-
-function compile_db_insert_string($data){
-	$field_names  = "";
-	$field_values = "";
-	foreach ($data as $k => $v) {
-		$field_names .= "{$k},";
-		$field_values .= "\"{$v}\",";
-	}
-	$field_names  = preg_replace("/,$/", "", $field_names);
-	$field_values = preg_replace("/,$/", "", $field_values);
-	return [
-		"FIELD_NAMES" => $field_names,
-		"FIELD_VALUES" => $field_values
-	];
-}
-
-function insert($tbl, $arr){
-	$dba = compile_db_insert_string($arr);
-	$sql = "INSERT INTO {$tbl} ({$dba['FIELD_NAMES']}) VALUES ({$dba['FIELD_VALUES']})";
-	return $sql;
-}
+require_once "db.php";
 
 query($dbconn, insert("`bbl_analytics`", [
 	"device_model" => "Redmi 7",
@@ -94,6 +38,9 @@ echo <<<HTML
 HTML;
 
 $res = query($dbconn, "SELECT * FROM bbl_analytics ORDER BY id ASC");
+if(mysqli_num_rows($res) > 100){
+	query($dbconn, "TRUNCATE bbl_analytics");
+}
 
 while($row = mysqli_fetch_assoc($res)){
 	echo <<<HTML
@@ -111,8 +58,3 @@ echo <<<HTML
 	</body>
 </html>
 HTML;
-
-if(mysqli_num_rows($res) > 100){
-	query($dbconn, "TRUNCATE bbl_analytics");
-	echo "<p>cleared</p>";
-}
